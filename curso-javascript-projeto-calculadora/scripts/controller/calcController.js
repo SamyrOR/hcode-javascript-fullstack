cfae325a52c14c15cbaec3b5c5e16d6e0784618d
Samyr <saamyr@live.com>
2020-06-29 23:02:21 -0300

    começando a realizar as operações matemáticas

--- curso-javascript-projeto-calculadora/scripts/controller/calcController.js
+++ curso-javascript-projeto-calculadora/scripts/controller/calcController.js
@@ -42,6 +42,10 @@ class CalcController {
         this._operation.pop();  
     };
     
+    getLastOperation () {
+        return this._operation[this._operation.length-1]
+    };
+    
     execBtn (value) {
         switch (value) {
             case 'ac':
@@ -51,22 +55,25 @@ class CalcController {
                 this.clearEntry();
                 break;
             case 'soma': 
-
+                    this.addOperation('+');
                 break;
             case 'subtracao': 
-                
+                    this.addOperation('-');
                 break;
             case 'divisao': 
-                
+                    this.addOperation('/');
                 break;
             case 'multiplicacao': 
-                
+                this.addOperation('*');
                 break;
             case 'porcento': 
-                
+            this.addOperation('%');
                 break;
             case 'igual': 
                 
+                break;
+            case 'ponto':
+            this.addOperation('.');
                 break;
             case '0':
             case '1':
@@ -82,10 +89,8 @@ class CalcController {
                 break;
             default:
                 this.setError();
-            
         }
     }
-
     initButtonsEvents () {
 
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
@@ -104,12 +109,38 @@ class CalcController {
         this.displayCalc = "Error"
     };
 
-    addOperation (value) {
-        this._operation.push(value);
-        console.log(this._operation);
+    isOperator (value) {
+        return (['+', '-', '*', '/','%'].indexOf(value) > -1);
     }
-    
-    
+
+    setLastOperation (value) {
+        this._operation[this._operation.length - 1] = value;
+    }
+
+    addOperation (value) {
+        if (isNaN(this.getLastOperation())) {
+            if(this.isOperator(value)){
+                this.setLastOperation(value)
+            }else if(isNaN(value)) {
+            
+            } else {
+                this._operation.push(value)
+            }
+               
+        } else {
+            if (this.isOperator(value)) {
+                this._operation.push(value)
+            } else {
+
+                let newValue = this.getLastOperation().toString() + value.toString();
+                this.setLastOperation(parseInt(newValue));
+                console.log(value);
+            }
+
+        }
+        console.log(this._operation);
+
+    }
 
     get displayTime () {
         return this._timeEl.innerHTML;
