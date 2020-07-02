d23a56fa406939fbe9ae2fe0b95f64024657432a
Samyr <saamyr@live.com>
2020-07-01 22:11:35 -0300

    usado forEach para seliconar inteligente cada input

---
+++ curso-javascript-projeto-usuarios/index.js
@@ -0,0 +1,13 @@
+let nome = document.querySelector('#exampleInputName');
+let gender = document.querySelectorAll('#form-user-create [name=gender]:checked');
+let birth = document.querySelector('#exampleInputBirth');
+let country = document.querySelector('#exampleInputCountry');
+let email = document.querySelector('#exampleInputEmail');
+let password = document.querySelector('#exampleInputPassword');
+let photo = document.querySelector('#exampleInputFile');
+let admin = document.querySelector('#exampleInputAdmin')
+
+let fields = document.querySelectorAll('#form-user-create [name]');
+fields.forEach(field => {
+    console.log(field.name)
+})
