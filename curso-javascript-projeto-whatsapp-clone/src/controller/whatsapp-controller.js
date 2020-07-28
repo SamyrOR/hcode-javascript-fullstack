f903e5e95994cbd47a78c9919a893f264bb77d2e
Samyr <saamyr@live.com>
2020-07-27 23:07:05 -0300

    adicionado os eventos de manipulação do menu lateral e iniciado os eventos do menuAttach

--- curso-javascript-projeto-whatsapp-clone/src/controller/whatsapp-controller.js
+++ curso-javascript-projeto-whatsapp-clone/src/controller/whatsapp-controller.js
@@ -45,8 +45,109 @@ class WhatsAppController {
         Element.prototype.hasClass = function(name){
             return this.classList.contains(name);
         }
+        HTMLFormElement.prototype.getForm = function () {
+            return new FormData(this);
+        }
+        HTMLFormElement.prototype.toJSON = function () {
+            let json = {};
+            this.getForm().forEach((value, key) => {
+                json[key] = value;
+            })
+            return json;
+        }
     }
+
+    closeAllLeftPanel(){
+        this.el.panelEditProfile.hide();
+        this.el.panelAddContact.hide();
+    }
+
+
     initEvents() {
-        
+        this.el.myPhoto.on('click', e=> {
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+            setTimeout(()=> {
+                this.el.panelEditProfile.addClass('open');
+            }, 100)
+        });
+        this.el.btnNewContact.on('click', e => {
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+            setTimeout(()=> {
+                this.el.panelAddContact.addClass('open');
+            }, 100)
+        });
+        this.el.btnClosePanelEditProfile.on('click', e=> {
+            this.el.panelEditProfile.removeClass('open')
+        })
+        this.el.btnClosePanelAddContact.on('click', e=> {
+            this.el.panelAddContact.removeClass('open')
+        })
+        this.el.photoContainerEditProfile.on('click', e=> {
+          this.el.inputProfilePhoto.click()  
+        })
+        this.el.inputNamePanelEditProfile.on('keypress', e=> {
+            if (e.key === 'Enter') {
+                e.preventDefault();
+                this.el.btnSavePanelEditProfile.click()
+            }
+        })
+        this.el.btnSavePanelEditProfile.on('click', e=> {
+            console.log(this.el.inputNamePanelEditProfile.innerHTML);
+        })
+        this.el.formPanelAddContact.on('submit', e=> {
+            e.preventDefault();
+            let formData = new FormData(this.el.formPanelAddContact);   
+        })
+        this.el.contactsMessagesList.querySelectorAll('.contact-item').forEach(item => {
+            item.on('click', e=> {
+                this.el.home.hide();
+                this.el.main.css({
+                    display:'flex'
+                })
+            })
+        })
+        this.el.btnAttach.on('click', e=> {
+            e.stopPropagation();
+            this.el.menuAttach.addClass('open');
+            document.addEventListener('click', this.closeMenuAttach.bind(this));
+        })
+        this.el.btnAttachPhoto.on('click', e=> {
+            this.el.inputPhoto.click();
+        });
+        this.el.inputPhoto.on('change', e=> {
+            console.log(this.el.inputPhoto.files);
+            [...this.el.inputPhoto.files].forEach(file => {
+                console.log(file);
+            })
+        })
+        this.el.btnAttachCamera.on('click', e=> {
+            this.el.panelMessagesContainer.hide();
+            this.el.panelCamera.addClass('open');
+            this.el.panelCamera.css({
+                'height':'calc(100% - 120px)'
+            });
+        });
+        this.el.btnClosePanelCamera.on('click', e=> {
+            this.el.panelCamera.removeClass('open');
+            this.el.panelMessagesContainer.show();
+        })
+        this.el.btnTakePicture.on('click', e=> {
+            console.log('take picture');
+        })
+        this.el.btnAttachDocument.on('click', e=> {
+            console.log('Document');
+        });
+        this.el.btnAttachContact.on('click', e=> {
+            console.log('Contact');
+        });
     }
+
+    closeMenuAttach(e){
+        this.el.menuAttach.removeClass('open');
+        document.removeEventListener('click', this.closeMenuAttach);
+        console.log('remove menu')
+    }
+
 }
