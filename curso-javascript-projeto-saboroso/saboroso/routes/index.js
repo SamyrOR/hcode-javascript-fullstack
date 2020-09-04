f739c1bfc536ac3f69e797d40ec2e931f3a7ca81
Samyr <saamyr@live.com>
2020-09-03 23:06:01 -0300

    orgaizado os arquivos html com include do ejs

--- curso-javascript-projeto-saboroso/saboroso/routes/index.js
+++ curso-javascript-projeto-saboroso/saboroso/routes/index.js
@@ -1,40 +1,48 @@
 var conn = require('./../inc/db');
 var express = require('express');
 var router = express.Router();
+var menus = require('./../inc/menus');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  conn.query(`
-    SELECT * FROM tb_menus ORDER BY title
-  `, (err, results) => {
-    if (err) {
-      console.log(err)
-    }
+  menus.getMenus().then(results => {
     res.render('index', {
       title: 'Restaurante Saboroso!',
-      menus: results
+      menus: results,
+      isHome: true
     })
   })
 });
 
 router.get('/contacts', function(req, res, next){
-  res.render('contacts',
-  {title: 'Contato - Restaurante Saboroso!'}
-  )
+  res.render('contacts',{
+    title: 'Contato - Restaurante Saboroso!',
+    background:'images/img_bg_3.jpg',
+    h1:'Diga um oi!'
+  })
 })
 router.get('/menus', function(req, res, next){
-  res.render('menus',
-  {title: 'Menu - Restaurante Saboroso!'}
-  )
+  menus.getMenus().then(results => {
+    res.render('menus',{
+      title: 'Menu - Restaurante Saboroso!',
+      background:'images/img_bg_1.jpg',
+      h1:'Saboreie nosso menu!!',
+      menus: results
+    })
+  })
 })
 router.get('/reservations', function(req, res, next){
-  res.render('reservations',
-  {title: 'Reservas - Restaurante Saboroso!'}
-  )
+  res.render('reservations',{
+    title: 'Reservas - Restaurante Saboroso!',
+    background:'images/img_bg_2.jpg',
+    h1:'Reserve uma Mesa!'
+  })
 })
 router.get('/services', function(req, res, next){
   res.render('services',{
-    title: 'Serviços - Restaurante Saboroso!'
+    title: 'Serviços - Restaurante Saboroso!',
+    background:'images/img_bg_1.jpg',
+    h1:'É um prazer poder servir!'
   })
 })
 
